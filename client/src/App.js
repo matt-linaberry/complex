902a025ba2afd2be1e9c6787e61eb33f6e550361
Matt Linaberry <mtlinaberry@gmail.com>
2018-10-08 21:29:15 -0400

    thru 121!
    thru 121

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
@@ -12,24 +12,16 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
+            <h1 className="App-title">Welcome to React</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
-              <Route exact path="/" component={Fib} />
-              <Route path="/otherpage" component={OtherPage} />
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
           </div>
-        </div>        
+        </div>
       </Router>
-
     );
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const keys = require('./keys');
 
-// Express app setup
-
+// Express App Setup
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -10,63 +9,62 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// create and connect to postgres
+// Postgres Client Setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
 });
+pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient.on('error', () => console.log('Lost Pg Connection'));
+pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+  .catch(err => console.log(err));
 
-pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch((error) => console.log(err));
-
-// connect to the redis instance
+// Redis Client Setup
 const redis = require('redis');
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
-
 const redisPublisher = redisClient.duplicate();
 
-// express route handlers
+// Express route handlers
 
 app.get('/', (req, res) => {
-    res.send('Hi');
+  res.send('Hi');
 });
 
 app.get('/values/all', async (req, res) => {
-    const values = await pgClient.query('SELECT * from values');
+  const values = await pgClient.query('SELECT * from values');
 
-    res.send(values.rows);
+  res.send(values.rows);
 });
 
-app.get('/values/current', async(req, res) => {
-    redisClient.hgetall('values', (err, values) => {
-        res.send(values);
-    });
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
 });
 
 app.post('/values', async (req, res) => {
-    const index = req.body.value;
+  const index = req.body.index;
 
-    if (parseInt(index) > 40) {
-        return res.status(422).send('Index too high');
-    }
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
 
-    redisClient.hset('values', index, 'Nothing yet');
-    redisPublisher.publish('insert', index);
-    pgClient.query('INSERT INTO values (number) values ($1)', [index]);
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-    res.send({working: true});
+  res.send({ working: true });
 });
 
 app.listen(5000, err => {
-    console.log('Listening');
-})
+  console.log('Listening');
+});
